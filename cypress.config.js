b9bf500efd493f43217fd6cac94106ac29c1f56f
celinaaarem <42302557+celinaaarem@users.noreply.github.com>
2023-12-26 12:48:22 -0800

    Task | ENG-119 | E2E TEST | global navigation (#3)
    * navigation tests
    
    * feat: main navigation test
    
    * moved breakpoints to config, added command to close attentive popup, updated mobile header links
    
    * moved breakpoints to config, added command to close attentive popup, …
    …updated mobile header links
    
    ---------
    
    Co-authored-by: Celina Oh <Celina.Oh@ad.dollskill.com>

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 2560,
+  viewportHeight: 1440,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  env: {
+    baseURL: 'https://dollskill.com',
+    mobileBreak: 767,
+    navBreak: 1025,
+    viewports: [
+      { device: 'iPhone', width: 390, height: 844 },
+      { device: 'iPad', width: 834, height: 1194 },
+      { device: 'Desktop', width: 1920, height: 1080 },
+      { device: 'Laptop', width: 2560, height: 1440 }
+    ]
+  }
 });
